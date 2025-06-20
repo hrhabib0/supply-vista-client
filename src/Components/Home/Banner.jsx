ae82e9e9f0e8249283f634d843269ab3197ac5e9
hrhabib0 <hrhabib398668@gmail.com>
2025-06-20 21:31:36 +0600

    add banner and implement swiper js

---
+++ src/Components/Home/Banner.jsx
@@ -0,0 +1,49 @@
+import React from 'react';
+import { Navigation, Pagination } from 'swiper/modules';
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/pagination';
+import 'swiper/css/navigation';
+
+const Banner = () => {
+    return (
+        <div className='h-[100px]'>
+            <Swiper
+                slidesPerView={1}
+                spaceBetween={30}
+                loop={true}
+                pagination={{
+                    clickable: true,
+                }}
+                navigation={true}
+                modules={[Pagination, Navigation]}
+                className="mySwiper h-[400px]"
+            >
+                <SwiperSlide>
+                    <div>
+                        <img src={'https://i.ibb.co/h1gG64cK/offer-one.png'}  alt="offer-one" className='w-full h-[400px]' />
+                    </div>
+                </SwiperSlide>
+                <SwiperSlide>
+                    <div>
+                        <img src={'https://i.ibb.co/h1gG64cK/offer-one.png'} alt="offer-one" className='w-full h-[400px]' />
+                    </div>
+                </SwiperSlide>
+                <SwiperSlide>
+                    <div>
+                        <img src={'https://i.ibb.co/h1gG64cK/offer-one.png'} alt="offer-one" className='w-full h-[400px]' />
+                    </div>
+                </SwiperSlide>
+                <SwiperSlide>
+                    <div>
+                        <img src={'https://i.ibb.co/h1gG64cK/offer-one.png'} alt="offer-one" className='w-full h-[400px]' />
+                    </div>
+                </SwiperSlide>
+            </Swiper>
+        </div>
+    );
+};
+
+export default Banner;
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import Banner from '../Components/Home/Banner';
 
 const Home = () => {
     return (
         <div>
-            This is home page. Page content is coming after sometimes
+            <Banner></Banner>
         </div>
     );
 };
