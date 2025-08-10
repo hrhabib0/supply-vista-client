796ff9dbcef47a96d4a154f180631fca494fd38e
hrhabib0 <hrhabib398668@gmail.com>
2025-08-10 15:56:24 +0600

    changed all categories page background color

--- src/Pages/AllCategories.jsx
+++ src/Pages/AllCategories.jsx
@@ -5,7 +5,7 @@ import { motion } from "motion/react"
 const AllCategories = () => {
     const categories = useLoaderData()
     return (
-        <div className="bg-black/40">
+        <div className="bg-gradient-to-r from-gray-50 to-blue-50">
             <div className="max-w-7xl mx-auto py-10 px-4 lg:px-0">
                 <h1 className="py-5 text-center font-bold text-4xl">Discover All Categories</h1>
                 <div className='grid gap-5 grid-cols-2 md:grid-cols-4'>
@@ -21,7 +21,7 @@ const AllCategories = () => {
                                 >
                                     <Link to={`/categories/${category.name}`}
                                     >
-                                        <div className='border border-gray-300 rounded-3xl p-2 flex flex-col gap-2 md:gap-4 items-center justify-center hover:text-blue-500 hover:scale-105 transition-transform duration-300'>
+                                        <div className='border border-gray-300 hover:border-[#1D4ED8] rounded-3xl p-2 flex flex-col gap-2 md:gap-4 items-center justify-center hover:text-[#1D4ED8] hover:scale-105 transition-transform duration-300'>
                                             <div className="avatar">
                                                 <div className="mask mask-heart w-16 md:w-24">
                                                     <img src={category.image} className="w-full" />
